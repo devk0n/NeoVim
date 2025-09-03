0172a6d1ee55574cd3eae147599b767cc6c2cf61
devkon <27301010+devk0n@users.noreply.github.com>
2025-09-03 16:43:30 +0200

    Added git and lualine

---
+++ lua/devkit/plugins/lualine.lua
@@ -0,0 +1,42 @@
+-- lualine.lua
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- icons
+    "lewis6991/gitsigns.nvim",     -- git signs
+  },
+  config = function()
+    require("gitsigns").setup() -- enable signs in gutter
+
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        always_divide_middle = false,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "|", right = "|" },
+      },
+      sections = {
+        lualine_a = {
+          { "mode", fmt = function(str) return " " .. str end }, -- Vim icon + mode
+        },
+        lualine_b = {
+          "branch", -- git branch
+          { "diff", source = require("gitsigns").diff_source }, -- fast diff from gitsigns
+        },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
+}
