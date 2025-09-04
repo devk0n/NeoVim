c5f41b5e04ece81be99d27b5e1996f7e73a7293b
devkon <27301010+devk0n@users.noreply.github.com>
2025-09-04 23:08:25 +0200

    Added README.md and treesitter

--- lua/devkit/plugins/snacks.lua
+++ lua/devkit/plugins/snacks.lua
@@ -1,43 +1,59 @@
-
 return {
   {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
-    opts = {},
+    opts = {
+      notifier = { enabled = true },
+      picker = { enabled = true },
+      dashboard = { enabled = true },    -- startup screen
+      explorer = { enabled = true },     -- file explorer (like Oil)
+      statuscolumn = { enabled = true }, -- nice number/diagnostic column
+
+      image = {
+        enabled = false,
+        mermaid = false,
+      },
+    },
     keys = {
       -- Snacks extras
-      { 
-        "<leader>lg", 
-        function() 
-          require("snacks").lazygit({ cwd = vim.fn.getcwd() }) 
-        end, 
-        desc = "Lazygit (repo root)" 
+      {
+        "<leader>lg",
+        function()
+          require("snacks").lazygit({ cwd = vim.fn.getcwd() })
+        end,
+        desc = "Lazygit (repo root)"
       },
-      { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "Lazygit Logs" },
+      { "<leader>gl", function() require("snacks").lazygit.log() end,        desc = "Lazygit Logs" },
       { "<leader>rN", function() require("snacks").rename.rename_file() end, desc = "Fast Rename Current File" },
-      { "<leader>dB", function() require("snacks").bufdelete() end, desc = "Delete or Close Buffer (Confirm)" },
+      { "<leader>dB", function() require("snacks").bufdelete() end,          desc = "Delete or Close Buffer (Confirm)" },
 
       -- Snacks pickers
-      { 
-        "<leader>pf", function() require("snacks").picker.files() end, 
-        desc = "Find Files (Snacks Picker)" 
+      {
+        "<leader>pf",
+        function() require("snacks").picker.files() end,
+        desc = "Find Files (Snacks Picker)"
       },
-      { 
-        "<leader>pc", function() require("snacks").picker.files({ cwd = "~/dotfiles/nvim/.config/nvim/lua" }) end, 
-        desc = "Find Config File" 
+      {
+        "<leader>pc",
+        function() require("snacks").picker.files({ cwd = "~/dotfiles/nvim/.config/nvim/lua" }) end,
+        desc = "Find Config File"
       },
-      { 
-        "<leader>ps", function() require("snacks").picker.grep() end, 
-        desc = "Grep word" 
+      {
+        "<leader>ps",
+        function() require("snacks").picker.grep() end,
+        desc = "Grep word"
       },
-      { 
-        "<leader>pws", function() require("snacks").picker.grep_word() end, 
-        desc = "Search Visual selection or Word", mode = { "n", "x" } 
+      {
+        "<leader>pws",
+        function() require("snacks").picker.grep_word() end,
+        desc = "Search Visual selection or Word",
+        mode = { "n", "x" }
       },
-      { 
-        "<leader>pk", function() require("snacks").picker.keymaps({ layout = "ivy" }) end, 
-        desc = "Search Keymaps (Snacks Picker)" 
+      {
+        "<leader>pk",
+        function() require("snacks").picker.keymaps({ layout = "ivy" }) end,
+        desc = "Search Keymaps (Snacks Picker)"
       },
     }
   }
